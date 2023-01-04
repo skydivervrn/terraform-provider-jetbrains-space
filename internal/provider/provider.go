058c0dbdbc5f8c6eb672146aab07ccd698c0f87f
aleksandr.boikov <aleksandr.boikov@jetbrains.com>
2023-01-04 19:32:11 +0200

    updated api client source

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	sc "github.com/hashicorp/terraform-provider-space/space-api-client"
+	sc "github.com/skydivervrn/space-api-client"
 )
 
 const (
