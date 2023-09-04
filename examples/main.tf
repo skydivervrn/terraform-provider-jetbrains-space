5f6d6c43fdb0d8cab470167e03d637398e6db6f5
aleksandr.boikov <aleksandr.boikov@jetbrains.com>
2023-09-04 17:32:53 +0200

    Updated registry manifest 2

--- examples/main.tf
+++ examples/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     jetbrains-space = {
-      version = "0.0.10"
+      version = "0.0.12"
       source  = "hashicorp.com/skydivervrn/jetbrains-space"
     }
   }
